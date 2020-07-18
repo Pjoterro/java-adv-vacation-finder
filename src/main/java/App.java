778b8d8025d08208a220c49f9942d74b3cc6e5d1
Pjoterro <piotr00twardowski@gmail.com>
2020-07-18 10:30:16 +0200

    git creation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-
+        List<String> test = DataLoader.fileLoad("TERC.csv");
+        System.out.println(test.toString());
     }
 }
--- src/main/java/DataLoader.java
+++ src/main/java/DataLoader.java
@@ -1,8 +1,19 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DataLoader {
 
-    /*public List<String> fileLoad(String filePath) {
-
-    }*/
+    public static List<String> fileLoad(String filePath) {
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak takiego pliku");
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(Arrays.asList(fileReader.toString().split("\\n")));
+    }
 }
