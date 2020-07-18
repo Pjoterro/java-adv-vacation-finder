b8175fc0fdd26e6151f836d32eb50da529d6b319
Pjoterro <piotr00twardowski@gmail.com>
2020-07-18 11:08:09 +0200

    file loader added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        List<String> test = DataLoader.fileLoad("TERC.csv");
-        System.out.println(test.toString());
+        List<String> test = DataLoader.loadFile("TERC.csv");
+        System.out.println(test);
+
     }
 }
--- src/main/java/DataLoader.java
+++ src/main/java/DataLoader.java
@@ -1,19 +1,31 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DataLoader {
 
-    public static List<String> fileLoad(String filePath) {
-        FileReader fileReader;
+    public static List<String> loadFile(String path) {
+        List<String> loadedRows = new ArrayList<>();
+        FileReader fileReader = null;
         try {
-            fileReader = new FileReader(filePath);
+            fileReader = new FileReader(path);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                loadedRows.add(line);
+            }
         } catch (FileNotFoundException e) {
-            System.out.println("Brak takiego pliku");
-            return new ArrayList<>();
+            e.printStackTrace();
+            System.out.println("File not found: " + path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error accessing file: " + path);
         }
-        return new ArrayList<>(Arrays.asList(fileReader.toString().split("\\n")));
+        return loadedRows;
     }
+
 }
